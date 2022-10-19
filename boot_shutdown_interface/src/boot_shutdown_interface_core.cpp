7ef4ce819edade01cb91ecbaca2e76217f676c7f
ito-san <fumihito.ito@tier4.jp>
2022-10-19 18:53:04 +0900

    Fixed issue
    Signed-off-by: ito-san <fumihito.ito@tier4.jp>

--- boot_shutdown_interface/src/boot_shutdown_interface_core.cpp
+++ boot_shutdown_interface/src/boot_shutdown_interface_core.cpp
@@ -14,118 +14,123 @@
 
 #include "boot_shutdown_interface/boot_shutdown_interface_core.hpp"
 
-#define FMT_HEADER_ONLY
+// #define FMT_HEADER_ONLY
 #include <fmt/format.h>
 
 namespace boot_shutdown_interface
 {
 
-BootShutdownInterface::BootShutdownInterface()
-: Node("boot_shutdown"),
-  ecu_name_(declare_parameter("ecu_name", std::string("ecu"))),
-  startup_timeout_(declare_parameter("startup_timeout", 300)),
-  preparation_timeout_(declare_parameter("preparation_timeout", 60)),
-  // Artificial delay for debugging
-  preparation_delay_(declare_parameter("debug.preparation_delay", 5))
-{
-  using namespace std::literals::chrono_literals;
-  using std::placeholders::_1;
-  using std::placeholders::_2;
+  BootShutdownInterface::BootShutdownInterface()
+      : Node("boot_shutdown"),
+        ecu_name_(declare_parameter("ecu_name", std::string("ecu"))),
+        startup_timeout_(declare_parameter("startup_timeout", 300)),
+        preparation_timeout_(declare_parameter("preparation_timeout", 60)),
+        // Artificial delay for debugging
+        preparation_delay_(declare_parameter("debug.preparation_delay", 5))
+  {
+    using namespace std::literals::chrono_literals;
+    using std::placeholders::_1;
+    using std::placeholders::_2;
 
-  ecu_state_.name = ecu_name_;
-  ecu_state_.state = EcuState::STARTUP;
-  startup_time_ = get_clock()->now();
+    ecu_state_.name = ecu_name_;
+    ecu_state_.state = EcuState::STARTUP;
+    startup_time_ = get_clock()->now();
 
-  srv_prepare_shutdown_ = create_service<PrepareShutdown>(
-    "~/service/prepare_shutdown", std::bind(
-      &BootShutdownInterface::onPrepareShutdown, this, _1,
-      _2),
-    rmw_qos_profile_default);
+    srv_prepare_shutdown_ = create_service<PrepareShutdown>(
+        "~/service/prepare_shutdown", std::bind(&BootShutdownInterface::onPrepareShutdown, this, _1, _2),
+        rmw_qos_profile_default);
 
-  srv_execute_shutdown_ = create_service<ExecuteShutdown>(
-    "~/service/execute_shutdown", std::bind(
-      &BootShutdownInterface::onExecuteShutdown, this, _1,
-      _2),
-    rmw_qos_profile_default);
+    srv_execute_shutdown_ = create_service<ExecuteShutdown>(
+        "~/service/execute_shutdown", std::bind(&BootShutdownInterface::onExecuteShutdown, this, _1, _2),
+        rmw_qos_profile_default);
 
-  pub_ecu_state_ =
-    create_publisher<EcuState>("~/output/ecu_state", rclcpp::QoS{1});
+    pub_ecu_state_ =
+        create_publisher<EcuState>("~/output/ecu_state", rclcpp::QoS{1});
 
-  timer_ = rclcpp::create_timer(
-    this, get_clock(), 1s, std::bind(&BootShutdownInterface::onTimer, this));
-}
-
-void BootShutdownInterface::onPrepareShutdown(
-  PrepareShutdown::Request::SharedPtr, PrepareShutdown::Response::SharedPtr response)
-{
-  RCLCPP_INFO(get_logger(), "shutdown preparing...");
-
-  // Clear pagecache, dentries, and inodes.
-  preparation_child_ = boost::process::child(
-    "/bin/sh", "-c",
-    fmt::format("sync; echo 3 > /proc/sys/vm/drop_caches; sleep {}", preparation_delay_));
-
-  ecu_state_.state = EcuState::SHUTDOWN_PREPARING;
-  preparation_start_time_ = get_clock()->now();
-
-  response->status.success = true;
-  response->power_off_time =
-    builtin_interfaces::msg::Time(get_clock()->now() + rclcpp::Duration(preparation_delay_, 0));
-}
-
-void BootShutdownInterface::onExecuteShutdown(
-  ExecuteShutdown::Request::SharedPtr, ExecuteShutdown::Response::SharedPtr response)
-{
-  RCLCPP_INFO(get_logger(), "shutdown executing...");
-
-  // Shutdown
-  // Avoid using "now" and delay shutdown to publish ecu_state
-  shutdown_child_ = boost::process::child("/bin/sh", "-c", fmt::format("shutdown -h now"));
-
-  auto power_off_time = builtin_interfaces::msg::Time(get_clock()->now());
-  ecu_state_.power_off_time = power_off_time;
-  response->status.success = true;
-  response->power_off_time = power_off_time;
-}
-
-void BootShutdownInterface::onTimer()
-{
-  if (ecu_state_.state == EcuState::STARTUP) {
-    if (isRunning()) {
-      ecu_state_.state = EcuState::RUNNING;
-    } else if (isStartupTimeout()) {
-      ecu_state_.state = EcuState::STARTUP_TIMEOUT;
-    }
-  } else if (ecu_state_.state == EcuState::SHUTDOWN_PREPARING) {
-    if (isReady()) {
-      ecu_state_.state = EcuState::SHUTDOWN_READY;
-    } else if (isPreparationTimeout()) {
-      ecu_state_.state = EcuState::SHUTDOWN_TIMEOUT;
-    }
+    timer_ = rclcpp::create_timer(
+        this, get_clock(), 1s, std::bind(&BootShutdownInterface::onTimer, this));
   }
 
-  pub_ecu_state_->publish(ecu_state_);
-}
+  void BootShutdownInterface::onPrepareShutdown(
+      PrepareShutdown::Request::SharedPtr, PrepareShutdown::Response::SharedPtr response)
+  {
+    RCLCPP_INFO(get_logger(), "shutdown preparing...");
 
-bool BootShutdownInterface::isRunning()
-{
-  // implement process to check for startup
-  return true;
-}
+    // Clear pagecache, dentries, and inodes.
+    preparation_child_ = boost::process::child(
+        "/bin/sh", "-c",
+        fmt::format("sync; echo 3 > /proc/sys/vm/drop_caches; sleep {}", preparation_delay_));
 
-bool BootShutdownInterface::isStartupTimeout()
-{
-  return (get_clock()->now() - startup_time_) > rclcpp::Duration(startup_timeout_, 0);
-}
+    ecu_state_.state = EcuState::SHUTDOWN_PREPARING;
+    preparation_start_time_ = get_clock()->now();
 
-bool BootShutdownInterface::isPreparationTimeout()
-{
-  return (get_clock()->now() - preparation_start_time_) > rclcpp::Duration(preparation_timeout_, 0);
-}
+    response->status.success = true;
+    response->power_off_time =
+        builtin_interfaces::msg::Time(get_clock()->now() + rclcpp::Duration(preparation_delay_, 0));
+  }
 
-bool BootShutdownInterface::isReady()
-{
-  return !(preparation_child_.running());
-}
+  void BootShutdownInterface::onExecuteShutdown(
+      ExecuteShutdown::Request::SharedPtr, ExecuteShutdown::Response::SharedPtr response)
+  {
+    RCLCPP_INFO(get_logger(), "shutdown executing...");
 
-}  // namespace boot_shutdown_interface
+    // Shutdown
+    // Avoid using "now" and delay shutdown to publish ecu_state
+    shutdown_child_ = boost::process::child("/bin/sh", "-c", fmt::format("shutdown -h now"));
+
+    auto power_off_time = builtin_interfaces::msg::Time(get_clock()->now());
+    ecu_state_.power_off_time = power_off_time;
+    response->status.success = true;
+    response->power_off_time = power_off_time;
+  }
+
+  void BootShutdownInterface::onTimer()
+  {
+    if (ecu_state_.state == EcuState::STARTUP)
+    {
+      if (isRunning())
+      {
+        ecu_state_.state = EcuState::RUNNING;
+      }
+      else if (isStartupTimeout())
+      {
+        ecu_state_.state = EcuState::STARTUP_TIMEOUT;
+      }
+    }
+    else if (ecu_state_.state == EcuState::SHUTDOWN_PREPARING)
+    {
+      if (isReady())
+      {
+        ecu_state_.state = EcuState::SHUTDOWN_READY;
+      }
+      else if (isPreparationTimeout())
+      {
+        ecu_state_.state = EcuState::SHUTDOWN_TIMEOUT;
+      }
+    }
+
+    pub_ecu_state_->publish(ecu_state_);
+  }
+
+  bool BootShutdownInterface::isRunning()
+  {
+    // implement process to check for startup
+    return true;
+  }
+
+  bool BootShutdownInterface::isStartupTimeout()
+  {
+    return (get_clock()->now() - startup_time_) > rclcpp::Duration(startup_timeout_, 0);
+  }
+
+  bool BootShutdownInterface::isPreparationTimeout()
+  {
+    return (get_clock()->now() - preparation_start_time_) > rclcpp::Duration(preparation_timeout_, 0);
+  }
+
+  bool BootShutdownInterface::isReady()
+  {
+    return !(preparation_child_.running());
+  }
+
+} // namespace boot_shutdown_interface
