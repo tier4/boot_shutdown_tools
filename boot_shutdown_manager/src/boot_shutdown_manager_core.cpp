55fb6c4d8562a09add7e931d54cb0db0dc3d33cc
ito-san <fumihito.ito@tier4.jp>
2024-10-23 22:29:23 +0900

    fix(boot_shutdown_manager): check for null response from cli_execute call
    Signed-off-by: ito-san <fumihito.ito@tier4.jp>

--- boot_shutdown_manager/src/boot_shutdown_manager_core.cpp
+++ boot_shutdown_manager/src/boot_shutdown_manager_core.cpp
@@ -182,6 +182,10 @@ void BootShutdownManager::onShutdownService(
     try {
       PrepareShutdownService req;
       auto resp = client->cli_prepare->call(req);
+      if (!resp) {
+        RCLCPP_WARN(get_logger(), "Empty response.");
+        continue;
+      }
     } catch (const std::runtime_error & e) {
       RCLCPP_WARN(get_logger(), "Error: %s", e.what());
     } catch (...) {
@@ -322,6 +326,10 @@ void BootShutdownManager::executeShutdown()
     try {
       ExecuteShutdownService req;
       auto resp = client->cli_execute->call(req);
+      if (!resp) {
+        RCLCPP_WARN(get_logger(), "Empty response.");
+        continue;
+      }
       rclcpp::Time power_off_time = convertToRclcppTime(resp.power_off_time);
       if (latest_power_off_time < power_off_time) {
         latest_power_off_time = power_off_time;
